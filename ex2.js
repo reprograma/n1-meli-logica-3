df92b197222a3d9cdceed4c756e7f3cc49dd5cab
natalya <natalya_peixoto@hotmail.com>
2019-10-02 09:23:20 -0300

    correção ex aula 2

--- ex2.js
+++ ex2.js
@@ -4,6 +4,17 @@
 // 11 12 13 14 15
 // 16 17 18 19 20
 // 21 22 23 24 25
+const arr = []
+let count = 1
+for (let i = 0; i < 5; i++) {
+  const linha = []
+  for (let j = 0; j < 5; j++) {
+    linha[j] = count
+    count++
+  }
+  arr[i] = linha
+}
+console.log(arr)
 
 // 2 - Agora inverta o loop e gere números de 25 a 1
 // [ [ 25, 24, 23, 22, 21 ],
@@ -11,6 +22,17 @@
 //   [ 15, 14, 13, 12, 11 ],
 //   [ 10, 9, 8, 7, 6 ],
 //   [ 5, 4, 3, 2, 1 ] ]
+const arr = []
+let count = 25
+for (let i = 0; i < 5; i++) {
+  const linha = []
+  for (let j = 0; j < 5; j++) {
+    linha[j] = count
+    count--
+  }
+  arr[i] = linha
+}
+console.log(arr)
 
 // 3 - Retorne o valor mais alto das somas entre o total de valors por linha, de uma array 4x4.
 //const arr4x4 = [[23, 56, 9, 0], [8, 24, 50, 5], [9, 10, 40, 7], [9, 48, 15, 2]]
@@ -19,6 +41,32 @@
 // 5 6 6 7
 // 9 9 9 9
 // Resultado esperado -> 36
+const arr = []
+const arr4x4 = [[23, 56, 9, 0], [8, 24, 50, 5], [9, 10, 40, 7], [9, 48, 15, 2]]
+for (let i = 0; i < arr4x4.length; i++) {
+  let total = arr4x4[i].reduce(function(a, b) {
+    return a + b
+  })
+  arr[i] = total
+}
+console.log(Math.max(...arr))
+
+//ou ...
+
+let soma = []
+for (let i = 0; i < arr4x4.length; i++) {
+  let linha = 0
+  for (let j = 0; j < arr4x4.length; j++) {
+    linha += arr4x4[i][j]
+  }
+  soma.push(linha)
+  console.log(soma)
+}
+console.log("o valor mais alto entre as somas é:", Math.max(...soma))
+
+
+
+
 
 // 4 - Recebemos dados do front-end, que foram salvos na const abaixo.
 // para salvar no banco de dados, primeiro, precisamos criar uma função que verifica
@@ -30,14 +78,35 @@
 // valores lowercase. Crie uma função que checa em uma array bidimensional se todas as strings
 // estão obedecendo as regras da tabela. E caso nao estejam, modifique a string para que possam ser
 // salvas no banco.
-// const dados = [
-//   ["Antonia Maria", "ar@gol.com", "Engenheira"],
-//   ["Joana Maranhão", "ar@gol.com", "Engenheira de software"],
-//   ["Juliana Paes", "argh@gol.com", "Advogada"],
-//   ["mariana DA SILVA", "mariana@gmail.com", "MEDICA"],
-//   ["MARINA BATISTA", "marina@gol.com", "professora"],
-//   ["Antonia Maria", "ari@gol.com", "cientista"]
-// ]
+let posicao = ""
+const dados = [
+  ["Antonia Maria", "ar@gol.com", "Engenheira"],
+  ["Joana Maranhão", "ar@gol.com", "Engenheira de software"],
+  ["Juliana Paes", "argh@gol.com", "Advogada"],
+  ["mariana DA SILVA", "mariana@gmail.com", "MEDICA"],
+  ["MARINA BATISTA", "marina@gol.com", "professora"],
+  ["Antonia Maria", "ari@gol.com", "cientista"]
+]
+for (let i=0;i<dados.length;i++){
+    for (let j =0;j<dados[i].length;j++){
+        posicao = dados[i].toLocaleString().toLowerCase().split(",")
+    }
+    console.log(posicao)
+}
+
+//ou ...
+const dadosOrganizados = []
+for (let i = 0; i < dados.length; i++) {
+  linha = []
+  for (let j = 0; j < dados[i].length; j++)__
+    linha.push(dados[i][j].toLowerCase())
+  }
+  dadosOrganizados.push(linha)
+}___
+console.log(dadosOrganizados)
+
+
+
 
 // 6 - Printe no console os números que são múltiplos de 3.
 //  - os que são multiplos de 3 e 5.
@@ -89,3 +158,5 @@
 // domínio: google.com
 // path: mail
 // parâmetros:user=fulano
+
+// http://pythontutor.com/javascript.html#mode=display
