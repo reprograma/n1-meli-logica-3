916e01bf4baa26999292c2dd77cd1fa727246c05
natalya <natalya_peixoto@hotmail.com>
2019-10-03 12:42:37 -0300

    correção ex aula 3

--- ex2.js
+++ ex2.js
@@ -4,35 +4,38 @@
 // 11 12 13 14 15
 // 16 17 18 19 20
 // 21 22 23 24 25
-const arr = []
-let count = 1
-for (let i = 0; i < 5; i++) {
-  const linha = []
-  for (let j = 0; j < 5; j++) {
-    linha[j] = count
-    count++
-  }
-  arr[i] = linha
-}
-console.log(arr)
 
+function matriz(linha, col, inicio) {
+  const arr = []
+  let count = inicio
+  for (let i = 0; i < linha; i++) {
+    const linha = []
+    for (let j = 0; j < col; j++) {
+      linha[j] = count
+      count++
+    }
+    arr[i] = linha
+  }
+  console.log(arr)
+}
+matriz(10, 3, 1)
 // 2 - Agora inverta o loop e gere números de 25 a 1
 // [ [ 25, 24, 23, 22, 21 ],
 //   [ 20, 19, 18, 17, 16 ],
 //   [ 15, 14, 13, 12, 11 ],
 //   [ 10, 9, 8, 7, 6 ],
 //   [ 5, 4, 3, 2, 1 ] ]
-const arr = []
-let count = 25
-for (let i = 0; i < 5; i++) {
-  const linha = []
-  for (let j = 0; j < 5; j++) {
-    linha[j] = count
-    count--
-  }
-  arr[i] = linha
-}
-console.log(arr)
+// const arr = []
+// let count = 25
+// for (let i = 0; i < 5; i++) {
+//   const linha = []
+//   for (let j = 0; j < 5; j++) {
+//     linha[j] = count
+//     count--
+//   }
+//   arr[i] = linha
+// }
+// console.log(arr)
 
 // 3 - Retorne o valor mais alto das somas entre o total de valors por linha, de uma array 4x4.
 //const arr4x4 = [[23, 56, 9, 0], [8, 24, 50, 5], [9, 10, 40, 7], [9, 48, 15, 2]]
@@ -41,72 +44,64 @@ console.log(arr)
 // 5 6 6 7
 // 9 9 9 9
 // Resultado esperado -> 36
-const arr = []
-const arr4x4 = [[23, 56, 9, 0], [8, 24, 50, 5], [9, 10, 40, 7], [9, 48, 15, 2]]
-for (let i = 0; i < arr4x4.length; i++) {
-  let total = arr4x4[i].reduce(function(a, b) {
-    return a + b
-  })
-  arr[i] = total
-}
-console.log(Math.max(...arr))
+// const arr = []
+// const arr4x4 = [[23, 56, 9, 0], [8, 24, 50, 5], [9, 10, 40, 7], [9, 48, 15, 2]]
+// for (let i = 0; i < arr4x4.length; i++) {
+//   let total = arr4x4[i].reduce(function(a, b) {
+//     return a + b
+//   })
+//   arr[i] = total
+// }
+// console.log(Math.max(...arr))
 
 //ou ...
 
-let soma = []
-for (let i = 0; i < arr4x4.length; i++) {
-  let linha = 0
-  for (let j = 0; j < arr4x4.length; j++) {
-    linha += arr4x4[i][j]
-  }
-  soma.push(linha)
-  console.log(soma)
-}
-console.log("o valor mais alto entre as somas é:", Math.max(...soma))
-
-
-
-
+// let soma = []
+// for (let i = 0; i < arr4x4.length; i++) {
+//   let linha = 0
+//   for (let j = 0; j < arr4x4.length; j++) {
+//     linha += arr4x4[i][j]
+//   }
+//   soma.push(linha)
+//   console.log(soma)
+// }
+// console.log("o valor mais alto entre as somas é:", Math.max(...soma))
 
 // 4 - Recebemos dados do front-end, que foram salvos na const abaixo.
 // para salvar no banco de dados, primeiro, precisamos criar uma função que verifica
 // se os valores são todos do type number.
 // Caso não sejam number, a função modifica os dados para que possam ser salvos corretamente.
 
-
 // 5 - Agora recebemos, alguns valores em string. Porém a nossa tabela no banco só aceita
 // valores lowercase. Crie uma função que checa em uma array bidimensional se todas as strings
 // estão obedecendo as regras da tabela. E caso nao estejam, modifique a string para que possam ser
 // salvas no banco.
-let posicao = ""
-const dados = [
-  ["Antonia Maria", "ar@gol.com", "Engenheira"],
-  ["Joana Maranhão", "ar@gol.com", "Engenheira de software"],
-  ["Juliana Paes", "argh@gol.com", "Advogada"],
-  ["mariana DA SILVA", "mariana@gmail.com", "MEDICA"],
-  ["MARINA BATISTA", "marina@gol.com", "professora"],
-  ["Antonia Maria", "ari@gol.com", "cientista"]
-]
-for (let i=0;i<dados.length;i++){
-    for (let j =0;j<dados[i].length;j++){
-        posicao = dados[i].toLocaleString().toLowerCase().split(",")
-    }
-    console.log(posicao)
-}
+// let posicao = ""
+// const dados = [
+//   ["Antonia Maria", "ar@gol.com", "Engenheira"],
+//   ["Joana Maranhão", "ar@gol.com", "Engenheira de software"],
+//   ["Juliana Paes", "argh@gol.com", "Advogada"],
+//   ["mariana DA SILVA", "mariana@gmail.com", "MEDICA"],
+//   ["MARINA BATISTA", "marina@gol.com", "professora"],
+//   ["Antonia Maria", "ari@gol.com", "cientista"]
+// ]
+// for (let i=0;i<dados.length;i++){
+//     for (let j =0;j<dados[i].length;j++){
+//         posicao = dados[i].toLocaleString().toLowerCase().split(",")
+//     }
+//     console.log(posicao)
+// }
 
 //ou ...
-const dadosOrganizados = []
-for (let i = 0; i < dados.length; i++) {
-  linha = []
-  for (let j = 0; j < dados[i].length; j++)__
-    linha.push(dados[i][j].toLowerCase())
-  }
-  dadosOrganizados.push(linha)
-}___
-console.log(dadosOrganizados)
-
-
-
+// const dadosOrganizados = []
+// for (let i = 0; i < dados.length; i++) {
+//   linha = []
+//   for (let j = 0; j < dados[i].length; j++)__
+//     linha.push(dados[i][j].toLowerCase())
+//   }
+//   dadosOrganizados.push(linha)
+// }___
+// console.log(dadosOrganizados)
 
 // 6 - Printe no console os números que são múltiplos de 3.
 //  - os que são multiplos de 3 e 5.
@@ -128,21 +123,45 @@ console.log(dadosOrganizados)
 //   [71, 72, 73, 74, 75]
 // ]
 
-//7- const cacaPalavras = [
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "l", "e", "f", "a", "n", "t", "e"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["c", "a", "s", "a", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "e", "d", "e", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"]
-// ]
+const cacaPalavras = [
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "l", "e", "f", "a", "n", "t", "e"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["c", "a", "s", "a", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "e", "d", "e", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"]
+]
 
-// const palavras = ["elefante", "casa", "rede"]
+const palavras = ["elefante", "casa", "rede"]
+
+for (let i = 0; i < cacaPalavras.length; i++) {
+  cacaPalavras[i] = cacaPalavras[i].join("")
+  for (let j = 0; j < palavras.length; j++) {
+    if (cacaPalavras[i].indexOf(palavras[j]) > -1) {
+      console.log(
+        `A palavra ${palavras[j]} está na linha ${i +
+          1} e começa na coluna ${cacaPalavras[i].indexOf(palavras[j]) + 1}`
+      )
+    }
+  }
+}
+
+//ou
+for (let i = 0; i < cacaPalavras.length; i++) {
+  for (let j = 0; j < palavras.length; j++) {
+    if (cacaPalavras[i].join("").indexOf(palavras[j]) > -1) {
+      console.log(
+        `a palavra ${palavras[j]} começa na linha ${i +
+          1}, na coluna ${cacaPalavras[i].join("").indexOf(palavras[j]) + 1}`
+      )
+    }
+  }
+}
 
 // ache as palavras da array palavras na array cacaPalavras e diga em qual linha e em qual coluna
 // elas estao.
--- ex3.js
+++ ex3.js
@@ -8,21 +8,45 @@
 // com pelo menos 4 propriedades e 1 método
 // ref: https://pt.wikipedia.org/wiki/Lista_de_Pok%C3%A9mon
 
+const pokemon = {
+  nome: "Charmander",
+  nomeJapones: "Hitokage",
+  numeroJohto: 234,
+  evoluiDe: "Ovo",
+  fraseFoda: function() {
+    return "chaaaaarrrrr-maaaannn-deeeeerr"
+  }
+}
+
 // 2.
 // Printe no console a seguinte
 // frase: "Oi, meu nome é  < >.
 // e meu nome japones é < >."
+
+pokemon.frase = function() {
+  return `Hello, my name is ${pokemon.nome}. And my name in Japanese ${pokemon.nomeJapones}.`
+}
+
+pokemon.frase()
 
 // 3.
 // Adicione uma nova propriedade ao
 // seu pokemon chamada `sabe_voar`
 // (ou algo do tipo) e defina
 // ela como `true` ou `false`.
+pokemon["sabe_voar"] = true
 
 // 4.
 // Adicione um método ao seu pokemon
 // chamado `voar` que retorna "Desculpa, nao sei voar." ou "
 // To voaaaaaando!".
+pokemon.fly = function() {
+  if (pokemon.sabe_voar) {
+    return `I'm flyyyyyying`
+  } else {
+    return "I can't fly"
+  }
+}
 
 // 5.
 // Adicione um método chamado
@@ -30,21 +54,39 @@
 // que retorna randomicamente uma
 // das Strings "FIGHT", "BAG" ou "RUN".
 
+pokemon["action"] = function() {
+  let frase = ["Fight", "Bag", "Run"]
+  let rand = Math.floor(Math.random() * frase.length)
+  return frase[rand]
+}
+
+pokemon.action()
+
 // 6.
 // Adicione um método chamado
 // `pergunta` ao seu pokemon que printa
 // no console "O que o  < > vai fazer?" e
 // retorna o resultado do metodo
 // `acao`.
+pokemon.ask = function() {
+  console.log(`What will ${pokemon.name} do?`)
+  return pokemon.do_something
+}
 
 // 7.
 // Printe todas as propriedades
 // do seu pokemon no console.
+for (const key in pokemon) {
+  console.log(key)
+}
 
 // 8.
 // Printe todas as propriedades
 // e seus respectivos valores no
 // console no seguinte formato:
-// <propriedade>: <valor
+// <propriedade>: <valor>
+for (const key in pokemon) {
+  console.log(`Chave:${key}| valor:${pokemon[key]}`)
+}
 
 //9 Faça uma função construtora de pokemon
