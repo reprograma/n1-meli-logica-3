fa5f60a0a032202d1dd0610a8b8ddf92ba27a2b6
jadyjacques <jadyjacques.nave@gmail.com>
2019-10-04 21:56:32 -0300

    exercicios semana 3

--- extra.js
+++ extra.js
@@ -1,27 +1,7 @@
 // use Map, filter, forEach, find e ou Reduce.
 
 //1
-// const numbers = [
-//   1,
-//   3,
-//   4,
-//   6,
-//   8,
-//   9,
-//   56,
-//   34,
-//   23,
-//   2,
-//   89,
-//   101,
-//   320,
-//   12,
-//   13,
-//   45,
-//   40,
-//   43,
-//   88
-// ]
+const numbers = [1,3,4,6,8,9,56,34,23,2,89,101,320,12,13,45,40,43,88]
 // retorne numeros multiplos de 3
 // retorne numeros multiplos de 3 e de 5
 
